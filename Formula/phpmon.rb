cbd943f55b530d454d084a8f7a75d293daa89a7e
Nico Verbruggen <nico.verb@gmail.com>
2020-07-11 10:10:45 +0200

    🚀 Initial version of `phpmon`

---
+++ Formula/phpmon.rb
@@ -0,0 +1,11 @@
+cask 'phpmon' do
+    version '2.0.0'
+    sha256 '412b11804b497d538f6dd6e158aa71d198e01f154b4de06456674d8fd3e1fb61'
+ 
+    url "https://github.com/nicoverbruggen/phpmon/releases/download/#{version}/phpmon.zip"
+    appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
+    name 'PHP Monitor'
+    homepage 'https://github.com/nicoverbruggen/phpmon'
+ 
+    app 'PHP Monitor.app'
+  end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Nico Verbruggen's Cask
+
+## How do I install these formulae?
+`brew install nicoverbruggen/homebrew-cask/<formula>`
+
+Or `brew tap nicoverbruggen/homebrew-cask` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
