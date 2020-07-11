ea96f60039c9dec0691b0c9086005c9e9d9feeb2
Nico Verbruggen <nico.verb@gmail.com>
2020-07-11 10:47:19 +0200

    ⬆️ Update to v2.1 of PHP Monitor

--- Casks/phpmon.rb
+++ Casks/phpmon.rb
@@ -1,8 +1,8 @@
 cask 'phpmon' do
-  version '2.0.0'
-  sha256 '412b11804b497d538f6dd6e158aa71d198e01f154b4de06456674d8fd3e1fb61'
+  version '2.1.0'
+  sha256 '5e7624d209b06cc953d592ea75f206fc9e1605051eb387201cfe97a4fd5ba79a'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v2.0/phpmon.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v2.1/phpmon.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor'
   homepage 'https://github.com/nicoverbruggen/phpmon'
