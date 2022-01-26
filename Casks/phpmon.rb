bf08c8b6c567e5a4a2f5fbf27a250b34e7f5c56d
Nico Verbruggen <mail@nicoverbruggen.be>
2022-01-27 00:58:04 +0100

    🚀 Updated PHP Monitor to v4.2.0

--- Casks/phpmon.rb
+++ Casks/phpmon.rb
@@ -1,9 +1,9 @@
 cask 'phpmon' do
   depends_on formula: 'gnu-sed'
-  version '4.1.2'
-  sha256 '900107003af4e42c1281e347037146e79b914b8b6d7d751d1ddef6eba01ce2f0'
+  version '4.2.0'
+  sha256 '1c185fb04f5dd320ea671dfa5de989aee3dfd374ce2de78e60f6b028c27825a5'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v4.1.2/phpmon.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v4.2.0/phpmon.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor'
   homepage 'https://github.com/nicoverbruggen/phpmon'
