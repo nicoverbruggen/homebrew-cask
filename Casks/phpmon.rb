b27a6c4dcd47c98ad3d9b1caccb770ad2e919fa6
Nico Verbruggen <mail@nicoverbruggen.be>
2022-02-09 00:04:56 +0100

    🚀 Updated PHP Monitor to v5.0.1

--- Casks/phpmon.rb
+++ Casks/phpmon.rb
@@ -1,9 +1,9 @@
 cask 'phpmon' do
   depends_on formula: 'gnu-sed'
-  version '5.0.0'
-  sha256 'd067837eec3e935a1e8436a2da74a66ea791e999a65bf8b6f5dec979a90444bf'
+  version '5.0.1'
+  sha256 'ad69a6bd7c999152a73c868aa5a0aff828dcc34090575546b928ac6fd7c60e0a'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.0/phpmon.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.0.1/phpmon.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor'
   homepage 'https://phpmon.app'
