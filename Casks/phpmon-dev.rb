2bc334cf87f5daf469fc4ea2b9d9788b88a1cc1e
Nico Verbruggen <mail@nicoverbruggen.be>
2022-04-23 13:25:35 +0200

    🚀 phpmon-dev 5.3 prerelease 1

--- Casks/phpmon-dev.rb
+++ Casks/phpmon-dev.rb
@@ -1,9 +1,9 @@
 cask 'phpmon-dev' do
   depends_on formula: 'gnu-sed'
-  version '5.2-dev2'
-  sha256 'fae5588182f88b45fc776de0724f3f6eb92d377e38334eb3f516c9f985ee735e'
+  version '5.3-dev'
+  sha256 'b238be224a58073a81e3026f37878ed59379ec7a56efe9cc995dd4ae1f5f1d80'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.2-dev/phpmon-5.2-prerelease-2.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.3-dev/phpmon-prerelease.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor DEV'
   homepage 'https://phpmon.app'
