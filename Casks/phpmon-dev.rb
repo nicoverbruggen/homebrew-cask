748c723882df9491837db7a25efdd704d208e27b
Nico Verbruggen <mail@nicoverbruggen.be>
2022-05-10 01:41:36 +0200

    🔧 Bump version number

--- Casks/phpmon-dev.rb
+++ Casks/phpmon-dev.rb
@@ -1,6 +1,6 @@
 cask 'phpmon-dev' do
   depends_on formula: 'gnu-sed'
-  version '5.3-dev'
+  version '5.3-dev.2'
   sha256 '4eb8afb3dda9a1a5573dccc9cf553972c2669d856cb75ac37b0c84042391b612'
 
   url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.3-dev/phpmon-prerelease-2.zip'
