7779c880a0d8bb48db57003a4d05990a72efd22e
Nico Verbruggen <mail@nicoverbruggen.be>
2022-05-10 18:22:57 +0200

    👌 Tweak build number

--- Casks/phpmon-dev.rb
+++ Casks/phpmon-dev.rb
@@ -1,9 +1,10 @@
 cask 'phpmon-dev' do
   depends_on formula: 'gnu-sed'
-  version '5.3-dev.2'
+
+  version '5.3-dev,781'
   sha256 '4eb8afb3dda9a1a5573dccc9cf553972c2669d856cb75ac37b0c84042391b612'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.3-dev/phpmon-prerelease-2.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.3-dev/phpmon-781.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor DEV'
   homepage 'https://phpmon.app'
