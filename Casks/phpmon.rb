0c471b0361076d58363f8246b3cc0d96bc749959
Nico Verbruggen <mail@nicoverbruggen.be>
2022-12-10 13:06:19 +0100

    🚀 phpmon 5.6.6, phpmon-dev 5.6.6 (992)

--- Casks/phpmon.rb
+++ Casks/phpmon.rb
@@ -1,10 +1,10 @@
 cask 'phpmon' do
   depends_on formula: 'gnu-sed'
 
-  version '5.6.5_991'
-  sha256 '30c9435ad5ef1186085ebd96122cd6cfb406c403e7550a2c6bcedcb9f58881bf'
+  version '5.6.6_992'
+  sha256 '7f78e117a34a2448b6cd9fc371204b9c5efc283168b63f55aa8ab2fc4d0b8401'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.6.5/phpmon.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.6.6/phpmon.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor'
   homepage 'https://phpmon.app'
