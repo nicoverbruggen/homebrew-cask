b1e32ed45903420e5cf4026b115722f8f02d5762
Nico Verbruggen <mail@nicoverbruggen.be>
2023-01-29 15:14:07 +0100

    🚀 phpmon 5.7.1, phpmon-dev 5.7.1 (1031)

--- Casks/phpmon-dev.rb
+++ Casks/phpmon-dev.rb
@@ -1,10 +1,10 @@
 cask 'phpmon-dev' do
   depends_on formula: 'gnu-sed'
 
-  version '5.7.0_1030'
-  sha256 '883174b8ed313507275ea276bde5e25cc041174b76f5ac5d1cc379ead5a9fbb8'
+  version '5.7.1_1031'
+  sha256 '545d4d230c60d8deddc2e2065aeb3de63aa510a0c4f459bbc00c1e2634ba9f07'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.7b5/phpmon-dev.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.7.1/phpmon-dev.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor DEV'
   homepage 'https://phpmon.app'
--- Casks/phpmon.rb
+++ Casks/phpmon.rb
@@ -1,10 +1,10 @@
 cask 'phpmon' do
   depends_on formula: 'gnu-sed'
 
-  version '5.7.0_1030'
-  sha256 'cc7cb1c28a08f7de32b7e1e1094b76d9bc1b0e33816279f73270f3431b6b2d50'
+  version '5.7.1_1031'
+  sha256 '58305b35fe80f5689a9e91a78ed53586e40a966a9f60f370fe81c53424b3250f'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.7/phpmon.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.7.1/phpmon.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor'
   homepage 'https://phpmon.app'
