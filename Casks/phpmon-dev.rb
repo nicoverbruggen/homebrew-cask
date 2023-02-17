4511a1867fd716319b885f10864bb8fa71e0ec71
Nico Verbruggen <mail@nicoverbruggen.be>
2023-02-17 17:25:14 +0100

    🚀 phpmon-dev 5.8.0 (1071)

--- Casks/phpmon-dev.rb
+++ Casks/phpmon-dev.rb
@@ -1,10 +1,10 @@
 cask 'phpmon-dev' do
   depends_on formula: 'gnu-sed'
 
-  version '5.8.0_1070'
-  sha256 '731e7b6d3f2342218d7ee9ffd364e4af9fb4dce821072896cd145bec290cb09b'
+  version '5.8.0_1071'
+  sha256 'dd33d4d4c7cc6bfaa3bdc3a999d374ee49a9563bd47dacac3d9a70b1659c3135'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.8b5/phpmon-dev.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.8b6/phpmon-dev.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor DEV'
   homepage 'https://phpmon.app'
