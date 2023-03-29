0f62d104559e77351973fb2b511031f2e6dd87f6
Nico Verbruggen <mail@nicoverbruggen.be>
2023-03-29 18:34:27 +0200

    🚀 phpmon, phpmon-dev 5.8.1 (1079)

--- Casks/phpmon-dev.rb
+++ Casks/phpmon-dev.rb
@@ -2,10 +2,10 @@ cask 'phpmon-dev' do
   depends_on formula: 'gnu-sed'
   depends_on macos: ">= :monterey"
 
-  version '5.8.0_1077'
-  sha256 '837084fd5e34ec8f00d3054cbf127aad69cfc85180abaeace985fb483dfa5366'
+  version '5.8.1_1079'
+  sha256 'b5595a2f9eea8911323a94b5a0c28ccd685b09d7d5a993e13fcfc426b6d65959'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.8/phpmon-dev.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.8.1/phpmon-dev.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor DEV'
   homepage 'https://phpmon.app'
--- Casks/phpmon.rb
+++ Casks/phpmon.rb
@@ -2,10 +2,10 @@ cask 'phpmon' do
   depends_on formula: 'gnu-sed'
   depends_on macos: ">= :monterey"
 
-  version '5.8.0_1077'
-  sha256 '8d0d381a12dd7d6bf66f070a1f44d34fb960631df3842c4ae978c2a88a25735f'
+  version '5.8.1_1079'
+  sha256 '804480a1b7d32846ac9e43d33609cfc53937b58d52a1343b33b40f4ad5fbf8b0'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.8/phpmon.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v5.8.1/phpmon.zip'
   appcast 'https://github.com/nicoverbruggen/phpmon/releases.atom'
   name 'PHP Monitor'
   homepage 'https://phpmon.app'
