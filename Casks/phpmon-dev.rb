f27f5956d75482fef9fd4080625d8fbc4eea415a
Nico Verbruggen <mail@nicoverbruggen.be>
2023-10-29 11:31:50 +0100

    🚀 phpmon 6.2.0, phpmon-dev 6.2.0 (1340)

--- Casks/phpmon-dev.rb
+++ Casks/phpmon-dev.rb
@@ -2,10 +2,10 @@ cask 'phpmon-dev' do
   depends_on formula: 'gnu-sed'
   depends_on macos: ">= :monterey"
 
-  version '6.1.0_1306'
-  sha256 'ecab250144dc4cfb5f6362153456dd3998d13856d29cc1945caf85c12261288e'
+  version '6.2.0_1340'
+  sha256 'bb96d2ec71d6476bd5f17af2221a07ecf64198d87ad534901e18ac357d1ee2e3'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v6.1/phpmon-dev.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v6.2/phpmon-dev.zip'
   name 'PHP Monitor DEV'
   homepage 'https://phpmon.app'
 
--- Casks/phpmon.rb
+++ Casks/phpmon.rb
@@ -2,10 +2,10 @@ cask 'phpmon' do
   depends_on formula: 'gnu-sed'
   depends_on macos: ">= :monterey"
 
-  version '6.1.0_1306'
-  sha256 '2b679dd839fcf6cabcd21c5be32a32fb7e7f9b02c198ec76b816f3f3db130104'
+  version '6.2.0_1340'
+  sha256 'c6c8a818f033db65c3f3ce30c4ac7bef8c7967234b990a4ee115cb160c7bc59a'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v6.1/phpmon.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v6.2/phpmon.zip'
   name 'PHP Monitor'
   homepage 'https://phpmon.app'
 
