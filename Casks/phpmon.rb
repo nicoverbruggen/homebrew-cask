ff17b38ae8ba28ab44010fa5652993032a528ea6
Nico Verbruggen <mail@nicoverbruggen.be>
2025-12-23 12:34:46 +0100

    🚀 phpmon 25.12.1 (1825)

--- Casks/phpmon.rb
+++ Casks/phpmon.rb
@@ -2,10 +2,10 @@ cask 'phpmon' do
   depends_on formula: 'gnu-sed'
   depends_on macos: ">= :ventura"
 
-  version '25.12_1810'
-  sha256 '69b1c896901802adfdce4e4576e923112b3cec2f907f8254267b1f39cfdcf2ee'
+  version '25.12.1_1825'
+  sha256 'cddaf2b1e08221069ef20e5f61752c228bd3edb650280b0f552300f6f2213ab8'
 
-  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v25.12/phpmon.zip'
+  url 'https://github.com/nicoverbruggen/phpmon/releases/download/v25.12.1/phpmon.zip'
   name 'PHP Monitor'
   homepage 'https://phpmon.app'
 
