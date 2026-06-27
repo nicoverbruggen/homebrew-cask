2ae58361ce85d74852e24bd8c73acd77e116c04d
Nico Verbruggen <mail@nicoverbruggen.be>
2026-06-27 10:07:21 +0200

    🔥 Fully disable phpmon-dev formula

--- Casks/phpmon-dev.rb
+++ Casks/phpmon-dev.rb
@@ -1,7 +1,7 @@
 cask 'phpmon-dev' do
-  deprecate! date: "2025-03-22", because: :unmaintained, replacement: "phpmon"
+  disable! date: "2025-03-22", because: :unmaintained, replacement_cask: "phpmon"
   depends_on formula: 'gnu-sed'
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   version '7.1.0_1525'
   sha256 '1cc0054f3f56e6811fc03d27df83fbbd76a5c5e2fefed1903beb408fc1b05b01'
